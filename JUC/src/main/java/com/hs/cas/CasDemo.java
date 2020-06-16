ae20d50e021b3a7a028b1a9719f256352ae1801e
RecursionHs <sknoendideal@163.com>
2020-06-16 22:49:15 +0800

    cas for atomicStampedReference

--- JUC/src/main/java/com/hs/cas/CasDemo.java
+++ JUC/src/main/java/com/hs/cas/CasDemo.java
@@ -17,6 +17,11 @@ public class CasDemo {
         // 期望、更新
         // public final boolean compareAndSet(int expect, int update)
         // 如果我期望的值达到了，那么就更新，否则，就不更新, CAS 是CPU的并发原语！
+        /**
+         *     public final boolean compareAndSet(int var1, int var2) {
+         *         return unsafe.compareAndSwapInt(this, valueOffset, var1, var2);
+         *     }
+         */
         atomicInteger.compareAndSet(2000,2001);
         System.out.println(atomicInteger.get());
 
